0c3158a09c2098e3b48cf1f40a2783da8d8fa258
Cristian <Cristian@DESKTOP-9RQ4IO4>
2022-03-31 20:22:21 +0200

    Prueba2 número negativo

--- src/sumador/ASumar.java
+++ src/sumador/ASumar.java
@@ -14,15 +14,48 @@ public class ASumar {
 	 * Metodo sumativo que comprueba si el número es de 1 dígito
 	 * @param cadena
 	 * @return String 
-	 **/
+	 */
 	
 	public String sumativo (String cadena) {
 	        String numero = cadena;
 	        String resultado= "";
 	        if (numero.length() == 1) {
 	        	resultado = numero + " = " + numero;
-	           
+	           System.out.println(resultado);
 	        } 
 	    return resultado;
 	}
+
+	/**
+	 * Metodo que comprueba si un número es negativo
+	 * @param cadena
+	 * @return String
+	 */
+	public String negativo(String cadena) {
+		 String numero = cadena;		 
+	     String resultado= "";
+	     char menor='-';			
+		if (numero.charAt(0)==menor) { 		 
+		  resultado= "negativo, siempre negativo";
+		  System.out.println(resultado);
+		}
+		return resultado;
+	}
+	
+	
+	
+
+/**
+ * @param args
+ */
+public static void main(String[] args) {
+	
+	 ASumar numero  = new ASumar();
+	
+	  numero.sumativo("5");
+	  numero.negativo("-54");
+	  
+	  
+}
+
 }
--- test/sumador/TestAsumar.java
+++ test/sumador/TestAsumar.java
@@ -11,13 +11,24 @@ import org.junit.Test;
 public class TestAsumar {
 
 	/**
-	 * Prueba para comprobar número de 1 cifra
+	 * Prueba para comprobar método sumativo
 	 */	
 	@Test
-	 public void testUnaCifra() {
+	 public void testsumativo() {
         ASumar sumo  = new ASumar();
         String resultado = sumo.sumativo("5");
         assertEquals("5 = 5",resultado );
         
+	}
+	
+	/**
+	 * Prueba para comprobar método negativo
+	 */
+	@Test
+	 public void testnegativo() {
+       ASumar sumo  = new ASumar();
+       String resultado = sumo.negativo("-54");
+       assertEquals("negativo, siempre negativo", resultado );
+       
 	}
 }
